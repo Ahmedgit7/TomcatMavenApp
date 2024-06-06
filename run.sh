54e5dc7791f3387bc225f1e7d17602d44d1dc3fe
Ahmedgit7 <131698234+Ahmedgit7@users.noreply.github.com>
2024-06-06 15:46:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ if [[ $run_sonar == "YES" ]]; then
   mvn clean verify sonar:sonar \
     -Dsonar.projectKey=Ahmed \
     -Dsonar.projectName='Ahmed' \
-    -Dsonar.host.url=http://13.235.18.63:9000 \
-    -Dsonar.token=squ_12da3eee5d706d8dab530428b3c4199715ffd846
+    -Dsonar.host.url=[ADD URL] \
+    -Dsonar.token=[ADD TOKEN]
 else
   echo "Running Maven without SonarQube"
   mvn clean verify
